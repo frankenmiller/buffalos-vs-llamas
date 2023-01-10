087ceeebe363db3a2fa793b2edf47365a6b50772
Frankenmiller <Dr.Frankenmiller@gmail.com>
2023-01-09 22:23:31 -0700

    Fixed github and linked in displays while in chinese mode

--- src/App.js
+++ src/App.js
@@ -43,11 +43,13 @@ export default function Board() {
 
   if (winner) {
     if (inChinese) {
-      status = winner + winner + winner + "赢了啊";
+      status = winner + winner + winner + "赢了";
       loser = (XisNext ? "🦬" : "🦙") + (XisNext ? "🦬" : "🦙") + "太烂了!";
       instructLineOne = "谢谢玩我创建的电子";
       instructLineTwo = "游戏 请点击你浏览器";
       instructLineThree = "刷新按钮来再玩🦬对🦙";    
+      linkedin = (XisNext ? "我想跟你创建程序" : "linkedin.com/in/frankenmiller");
+      github = (XisNext ? "https://github.com/frankenmiller" : "让我们一起创建顺利的程序")      
     } else {
       instructLineOne = "Thank you for testing my";
       instructLineTwo = "game! Please hit browser";
